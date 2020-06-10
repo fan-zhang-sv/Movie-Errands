fd628ebe6881b867623280dfe55bd219aebe764d
Fan Zhang <felicis_zhang@outlook.com>
2020-06-09 21:08:57 -0700

    Initial Commit

---
+++ Movie Errands/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Errands
+//
+//  Created by Fan Zhang on 6/9/20.
+//  Copyright © 2020 Fan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
